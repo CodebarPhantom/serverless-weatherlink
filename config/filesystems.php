2fe04eb350d917959ee43bd0dbf2df7687a5a005
CodebarPhantom <eryangamer10@gmail.com>
2022-10-27 09:10:51 +0700

    feat: config s3 public

--- config/filesystems.php
+++ config/filesystems.php
@@ -14,6 +14,7 @@ return [
     */
 
     'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'public' => env('FILESYSTEM_DRIVER_PUBLIC', 'public_local'),
 
     /*
     |--------------------------------------------------------------------------
@@ -35,7 +36,8 @@ return [
             'root' => storage_path('app'),
         ],
 
-        'public' => [
+       // 'public' => [
+        'public_local' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
@@ -46,11 +48,22 @@ return [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'token' => env('AWS_SESSION_TOKEN'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        ],
+
+        's3_public' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'token' => env('AWS_SESSION_TOKEN'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_PUBLIC_BUCKET'),
+            'url' => env('AWS_URL'),
         ],
 
     ],
