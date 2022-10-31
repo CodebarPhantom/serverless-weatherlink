bae2193bd012637b4a4a5a0a367edb2c31ae0dbd
CodebarPhantom <eryangamer10@gmail.com>
2022-10-31 07:57:03 +0700

    fix: not store excel

--- resources/views/exports/weather-history-template.blade.php
+++ resources/views/exports/weather-history-template.blade.php
@@ -15,7 +15,7 @@
     @foreach($datas as $data)
         <tr>
             <td>{{ $no++ }}</td>
-            <td>{{ $data->rain_rate_hi_mm == 0 ? "0" : $weatherHistory->rain_rate_hi_mm }}</td>
+            <td>{{ $data->rain_rate_hi_mm == 0 ? "0" : $data->rain_rate_hi_mm }}</td>
             <td>{{ Carbon::createFromTimestamp($data->unix_epoch_time)->format('d/m/Y H:i') }}</td>
         </tr>
     @endforeach
