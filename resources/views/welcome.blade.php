c13f76535d57a853305791c91430e512de8020b4
CodebarPhantom <eryangamer10@gmail.com>
2022-12-15 15:38:00 +0700

    fix: disable vite production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link rel="icon" type="image/x-icon" href="https://suryacipta.com/wp-content/uploads/2021/09/va.png" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
         <style>
             .text-header {
                 font-family: "Merriweather", sans-serif;
