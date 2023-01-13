b07b3691bb5e3cb3eb45aafbbd0a56e17975d9da
CodebarPhantom <eryangamer10@gmail.com>
2023-01-13 09:23:53 +0700

    style: dashboard
    fix: cronjob after 5 minuts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,10 @@
 
             </div>
             <div class="row">
-                <div class="col-7">
+                <div class="col-sm-12 col-md-5 order-md-2 order-sm-1 order-xs-1" style="margin: auto;">
+                    <canvas  id="myChart" height="280px"></canvas>
+                </div>
+                <div class="col-sm-12 col-md-7 order-md-1 order-sm-2 order-xs-2">
                     <iframe
                         src="https://www.weatherlink.com/embeddablePage/show/86da60c806c44759b79af156b6648793/fullscreen"
                         style="
@@ -62,9 +65,7 @@
                         ">
                     </iframe>
                 </div>
-                <div class="col-5" style="margin: auto;">
-                    <canvas  id="myChart" height="100%"></canvas>
-                </div>
+
             </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
@@ -85,7 +86,7 @@
                     borderWidth: 2,
                     borderRadius: 5,
                     borderSkipped: false,
-                    minBarLength: 7,
+                    minBarLength: 2,
                 },
                 {
                     label: 'Rain Fall (mm)',
@@ -95,7 +96,7 @@
                     borderWidth: 2,
                     borderRadius: 5,
                     borderSkipped: false,
-                    minBarLength: 7,
+                    minBarLength: 2,
                 }
             ]
         };
